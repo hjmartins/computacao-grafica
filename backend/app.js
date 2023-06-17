b179100630f8d8db51f3dbe05642c4596e5a4087
Marinho <joaovm048@gmail.com>
2023-06-17 12:30:53 -0300

    base para processamento de imagens

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 const RoutesFiguras = require('./routes/figura.routes')
-const RoutesTransformacoes = require('./routes/transformaçõesFiguras.routes')
+const RoutesTransformacoesFiguras = require('./routes/transformaçõesFiguras.routes')
 const RoutesFiltros = require('./routes/filtros.routes')
+const RoutesOperadoresMorfologicos = require('./routes/operadoresMorfologicos.routes')
+const RoutesTransformacoesImagens = require('./routes/transformacaoImagem.routes')
 
 //ativa CORS para possibilitar usar servidor e fron-end na mesma maquina
 app.use(function (req, res, next) {
@@ -25,8 +27,10 @@ app.get("/", (req, res) => {
 });
 
 app.use(RoutesFiguras);
-app.use(RoutesTransformacoes);
+app.use(RoutesTransformacoesFiguras);
 app.use(RoutesFiltros);
+app.use(RoutesOperadoresMorfologicos);
+app.use(RoutesTransformacoesImagens);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
