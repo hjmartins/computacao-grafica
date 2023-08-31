38b09d81dc658b84133f60687c96370a0cacfe6d
AdminUser <thiagofelipe1311@gmail.com>
2023-08-31 11:12:41 -0300

    Thigas att total

--- frontend/src/components/PagesMenu.js
+++ frontend/src/components/PagesMenu.js
@@ -2,7 +2,7 @@ const PagesMenu = {
     HOME: { key: '', title: 'Home' },
     ALUNOS: { key: 'sobre', title: 'Alunos' },
     PROJETO1: { key: 'projeto', title: 'Projeto 1'},
-    // PROJETO2: { key: 'projeto', title: 'Projeto 2'}
+    PROJETO2: { key: 'projeto', title: 'Projeto 2'}
 };
 
 module.exports = PagesMenu;
