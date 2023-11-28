bb8abbc2a9279aa3aee9e6c975e31deff30c904b
Luana <luanadevops@gmail.com>
2023-11-28 08:33:22 -0300

    add matriz identidade

--- backend/util/matrizes.js
+++ backend/util/matrizes.js
@@ -1,22 +1,28 @@
 class Matrizes{
-
-  multiplicaMatriz(matriz1, matriz2) {
-    const rows1 = matriz1.length;
-    const cols1 = matriz1[0].length;
-    const rows2 = matriz2.length;
-    const cols2 = matriz2[0].length;
-  
-    if (cols1 !== rows2) {
-      throw new Error('As matrizes não podem ser multiplicadas. O número de colunas da matriz1 deve ser igual ao número de linhas da matriz2.');
+  identidade(size) {
+    const result = [];
+    for (let i = 0; i < size; i++) {
+      const row = [];
+      for (let j = 0; j < size; j++) {
+        row.push(i === j ? 1 : 0);
+      }
+      result.push(row);
     }
+    return result;
+  }
+  multiplicaMatriz(matrizA, matrizB) {
+    const rowsA = matrizA.length;
+    const colsA = matrizA[0].length;
+    const colsB = matrizB[0].length;
   
     const result = [];
-    for (let i = 0; i < rows1; i++) {
+  
+    for (let i = 0; i < rowsA; i++) {
       result[i] = [];
-      for (let j = 0; j < cols2; j++) {
+      for (let j = 0; j < colsB; j++) {
         result[i][j] = 0;
-        for (let k = 0; k < cols1; k++) {
-          result[i][j] += matriz1[i][k] * matriz2[k][j];
+        for (let k = 0; k < colsA; k++) {
+          result[i][j] += matrizA[i][k] * matrizB[k][j];
         }
       }
     }
